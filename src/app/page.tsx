3e4551a72b2ffa96bf2d7fd0bbc9962de89762bf
vexdreal <anggaizzy05@gmail.com>
2026-07-22 17:32:06 +0700

    Redirect homepage to admin login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <div className="login-card">
-        <h1>VexdHub</h1>
-
-        <p>Premium Key System Platform</p>
-
-        <input 
-          type="text" 
-          placeholder="Username"
-        />
-
-        <input 
-          type="password" 
-          placeholder="Password"
-        />
-
-        <button>
-          Login
-        </button>
-      </div>
-    </main>
-  );
+  redirect("/admin");
 }
